ec696639006f7a1499360b108c09c2415c3021f4
Slava Knyazev <slava@knyazev.io>
2021-12-26 16:09:23 -0500

    messing around

--- a_native_example.cc
+++ a_native_example.cc
@@ -1,15 +1,24 @@
 #include <nan.h>
 #include <stdio.h>
+#include <clip.h>
+#include <stdlib.h>
+#include <iostream>
 
 using namespace v8;
 
-NAN_METHOD(Print) {
-  if (!info[0]->IsString()) return Nan::ThrowError("Must pass a string");
+NAN_METHOD(Print)
+{
+  if (!info[0]->IsString())
+    return Nan::ThrowError("Must pass a string");
   Nan::Utf8String path(info[0]);
-  printf("Printed from C++: %s\n", *path);
+  std::string value;
+  clip::get_text(value);
+  std::cout << value << "\n";
+  printf("Printed from C++ Test: %s\n", *path);
 }
 
-NAN_MODULE_INIT(InitAll) {
+NAN_MODULE_INIT(InitAll)
+{
   Nan::Set(target, Nan::New<String>("print").ToLocalChecked(), Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
 }
 
